33fa76937bf57fb45caffc643fc9dab67fecbbfa
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-10-01 16:49:24 +0700

    Initial commit

---
+++ src/FirstSample.java
@@ -0,0 +1,8 @@
+/**
+ * Created by anonymous.vn1985@gmail.com
+ */
+public class FirstSample {
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello World !'");
+    }
+}
