7bc7e1e747afd18cd20f57c99af8a0d13df1f18b
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-10-04 00:16:34 +0700

    New Commit

---
+++ src/Example_2.java
@@ -0,0 +1,14 @@
+/**
+ * Created by anonymous.vn1985@gmail.com
+ */
+public class Example_2 {
+    public static void main(String[] args) {
+        int var1;
+        int var2;
+        var1 = 1024;
+        System.out.println("var1 contains " + var1);
+        var2 = var1 / 2;
+        System.out.print("var2 contains var1 / 2: ");
+        System.out.println(var2);
+    }
+}
--- src/HandlingSyntaxError.java
+++ src/HandlingSyntaxError.java
@@ -1,8 +1,8 @@
 /**
  * Created by anonymous.vn1985@gmail.com
  */
-public class HandlingSyntaxError {
+/*public class HandlingSyntaxError {
     public static void main(String[] args)
 
     }
-}
+}*/
